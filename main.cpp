4f239b7289da59dde4cb6eced7d7e4a522c546a0
Saishravan Muthukrishnan <saishravanm@gmail.com>
2023-01-05 15:54:41 -0600

    Working cursor, and beginning stages of map editor.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include<SFML/Graphics.hpp>
 #include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/Rect.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/Sprite.hpp>
 #include <SFML/System/Vector2.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/Keyboard.hpp>
 #include <SFML/Window/Mouse.hpp>
 #include <SFML/Window/Window.hpp>
 #include <iostream>
@@ -24,13 +28,15 @@ int main(int argc, char * argv[])
 	
 	Renderer renderer;
 	sf::Vector2f origin(200,100);
-	sf::Vector2i mouseOrigin(200,100);
+	sf::Vector2f mouseOrigin(200,100);
 	sf::Color defColor(0,0,0);
 	sf::Color selectedColor(50,168,82);
 	Square grid[20][20];
 	renderer.plotGrid(grid, origin);
-	sf::Sprite cursorSprite;
-	Entity cursor(mouseOrigin,"cursor.png",cursorSprite,"Cursor");
+	//sf::Sprite cursorSprite;
+	//Entity cursor(mouseOrigin,"cursor.png",cursorSprite,"Cursor");
+	Square cursor(mouseOrigin,5,sf::Color::Green,sf::Color::Green);
+	bool mapEdit = false;
 	while (window.isOpen())
 	{
 		
@@ -42,18 +48,53 @@ int main(int argc, char * argv[])
 				window.close();
 			}
 			
+			
+			
+			
 		}
-		cursor.setPos(sf::Mouse::getPosition(window));
-		window.draw(cursorSprite);
+		cursor.setPosition(mouseOrigin);
+		mouseOrigin = sf::Vector2f(sf::Mouse::getPosition(window).x-10,sf::Mouse::getPosition(window).y-10);
 		window.clear(sf::Color(0,0,0));
 		for(int r = 0;  r < 20; r++)
 		{
+
 			for(int c = 0; c < 20; c++)
 			{	
 				//grid[r][c] = Square(origin,20,sf::Color(255,255,255),defColor);
+				/*
+				if(renderer.renderSquare(cursor).getGlobalBounds().intersects(renderer.renderSquare(grid[r][c]).getGlobalBounds()))
+				{
+					grid[r][c].setFill(sf::Color::Green);
+				}
+				*/
+				if(sf::Keyboard::isKeyPressed(sf::Keyboard::M))
+				{
+					mapEdit = !mapEdit;
+				}
+				if(mapEdit == true)
+				{
+				if(renderer.renderSquare(cursor).getGlobalBounds().intersects(renderer.renderSquare(grid[r][c]).getGlobalBounds()))
+				{
+					grid[r][c].setFill(sf::Color::Green);
+				}
+				}
+				/*
+				else if(!sf::Mouse::isButtonPressed(sf::Mouse::Left))
+				{
+					grid[r][c].setFill(sf::Color::Black);
+				}
+				
+				*/
+				
 				window.draw(renderer.renderSquare(grid[r][c]));
+				if(mapEdit == false || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+				{
+					grid[r][c].setFill(sf::Color::Black);
+
+				}
 			}
 		}		
+		window.draw(renderer.renderSquare(cursor));
 		window.display();
 
 	}
